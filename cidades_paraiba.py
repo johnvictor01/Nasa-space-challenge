da6b7e532d4e437d4cae420a6ccfd6a0c4bcea65
johnvictor01 <johnpazzz01@gmail.com>
2025-10-05 22:56:03 -0300

    Upload APIs

---
+++ cidades_paraiba.py
@@ -0,0 +1,896 @@
+# arquivo gerado automaticamente
+cidades_paraiba = \
+{
+  "paraiba-aguabranca": {
+    -7.515105,
+    -37.639403
+  },
+  "paraiba-aguiar": {
+    -7.09284,
+    -38.172722
+  },
+  "paraiba-alagoagrande": {
+    -7.041521,
+    -35.628532
+  },
+  "paraiba-alagoanova": {
+    -7.060115,
+    -35.761457
+  },
+  "paraiba-alagoinha": {
+    -6.948031,
+    -35.545793
+  },
+  "paraiba-alcantil": {
+    -7.741342,
+    -36.065357
+  },
+  "paraiba-algodaodejandaira": {
+    -6.904018,
+    -36.008988
+  },
+  "paraiba-alhandra": {
+    -7.430425,
+    -34.91061
+  },
+  "paraiba-saojoaodoriodopeixe": {
+    -6.729105,
+    -38.447874
+  },
+  "paraiba-amparo": {
+    -7.569128,
+    -37.06414
+  },
+  "paraiba-aparecida": {
+    -6.78596,
+    -38.083135
+  },
+  "paraiba-aracagi": {
+    -6.851997,
+    -35.38356
+  },
+  "paraiba-arara": {
+    -6.82719,
+    -35.76057
+  },
+  "paraiba-araruna": {
+    -6.532115,
+    -35.739759
+  },
+  "paraiba-areia": {
+    -6.968024,
+    -35.699478
+  },
+  "paraiba-areiadebaraunas": {
+    -7.123395,
+    -36.944622
+  },
+  "paraiba-areial": {
+    -7.050131,
+    -35.924524
+  },
+  "paraiba-aroeiras": {
+    -7.54629,
+    -35.708
+  },
+  "paraiba-assuncao": {
+    -7.073396,
+    -36.7274
+  },
+  "paraiba-baiadatraicao": {
+    -6.691258,
+    -34.937124
+  },
+  "paraiba-bananeiras": {
+    -6.753768,
+    -35.633834
+  },
+  "paraiba-barauna": {
+    -6.643394,
+    -36.253193
+  },
+  "paraiba-barradesantana": {
+    -7.523086,
+    -35.998852
+  },
+  "paraiba-barradesantarosa": {
+    -6.721801,
+    -36.056464
+  },
+  "paraiba-barradesaomiguel": {
+    -7.752809,
+    -36.3171
+  },
+  "paraiba-bayeux": {
+    -7.130163,
+    -34.936532
+  },
+  "paraiba-belem": {
+    -6.696759,
+    -35.536963
+  },
+  "paraiba-belemdobrejodocruz": {
+    -6.188073,
+    -37.533227
+  },
+  "paraiba-bernardinobatista": {
+    -6.453217,
+    -38.549542
+  },
+  "paraiba-boaventura": {
+    -7.420246,
+    -38.215678
+  },
+  "paraiba-boavista": {
+    -7.262071,
+    -36.241516
+  },
+  "paraiba-bomjesus": {
+    -6.815473,
+    -38.655468
+  },
+  "paraiba-bomsucesso": {
+    -6.445895,
+    -37.929766
+  },
+  "paraiba-bonitodesantafe": {
+    -7.314333,
+    -38.514668
+  },
+  "paraiba-boqueirao": {
+    -7.487004,
+    -36.130887
+  },
+  "paraiba-igaracy": {
+    -7.157113,
+    -38.136535
+  },
+  "paraiba-borborema": {
+    -6.806672,
+    -35.599019
+  },
+  "paraiba-brejodocruz": {
+    -6.350564,
+    -37.494376
+  },
+  "paraiba-brejodossantos": {
+    -6.376937,
+    -37.824312
+  },
+  "paraiba-caapora": {
+    -7.510234,
+    -34.915411
+  },
+  "paraiba-cabaceiras": {
+    -7.488993,
+    -36.286611
+  },
+  "paraiba-cabedelo": {
+    -6.97324,
+    -34.835161
+  },
+  "paraiba-cachoeiradosindios": {
+    -6.925919,
+    -38.674216
+  },
+  "paraiba-cacimbadeareia": {
+    -7.128851,
+    -37.157519
+  },
+  "paraiba-cacimbadedentro": {
+    -6.64251,
+    -35.79293
+  },
+  "paraiba-cacimbas": {
+    -7.212545,
+    -37.05934
+  },
+  "paraiba-caicara": {
+    -6.614687,
+    -35.468143
+  },
+  "paraiba-cajazeiras": {
+    -6.889785,
+    -38.557039
+  },
+  "paraiba-cajazeirinhas": {
+    -6.961442,
+    -37.799373
+  },
+  "paraiba-caldasbrandao": {
+    -7.102796,
+    -35.325468
+  },
+  "paraiba-camalau": {
+    -7.885031,
+    -36.824191
+  },
+  "paraiba-campinagrande": {
+    -7.224674,
+    -35.877129
+  },
+  "paraiba-capim": {
+    -6.917409,
+    -35.172033
+  },
+  "paraiba-caraubas": {
+    -7.727323,
+    -36.492336
+  },
+  "paraiba-carrapateira": {
+    -7.046931,
+    -38.3386
+  },
+  "paraiba-casserengue": {
+    -6.779538,
+    -35.817875
+  },
+  "paraiba-catingueira": {
+    -7.120081,
+    -37.60638
+  },
+  "paraiba-catoledorocha": {
+    -6.340625,
+    -37.746967
+  },
+  "paraiba-caturite": {
+    -7.419965,
+    -36.020713
+  },
+  "paraiba-conceicao": {
+    -7.556506,
+    -38.505371
+  },
+  "paraiba-condado": {
+    -6.90921,
+    -37.596637
+  },
+  "paraiba-conde": {
+    -7.260036,
+    -34.905352
+  },
+  "paraiba-congo": {
+    -7.79576,
+    -36.660213
+  },
+  "paraiba-coremas": {
+    -7.01759,
+    -37.946506
+  },
+  "paraiba-coxixola": {
+    -7.627292,
+    -36.606013
+  },
+  "paraiba-cruzdoespiritosanto": {
+    -7.139022,
+    -35.085721
+  },
+  "paraiba-cubati": {
+    -6.864797,
+    -36.352599
+  },
+  "paraiba-cuite": {
+    -6.485507,
+    -36.147861
+  },
+  "paraiba-cuitegi": {
+    -6.896278,
+    -35.522885
+  },
+  "paraiba-cuitedemamanguape": {
+    -6.912922,
+    -35.250219
+  },
+  "paraiba-curraldecima": {
+    -6.717416,
+    -35.266704
+  },
+  "paraiba-curralvelho": {
+    -7.537647,
+    -38.197913
+  },
+  "paraiba-damiao": {
+    -6.632163,
+    -35.904926
+  },
+  "paraiba-desterro": {
+    -7.28897,
+    -37.088076
+  },
+  "paraiba-vistaserrana": {
+    -6.739166,
+    -37.568576
+  },
+  "paraiba-diamante": {
+    -7.426183,
+    -38.264688
+  },
+  "paraiba-donaines": {
+    -6.603224,
+    -35.627146
+  },
+  "paraiba-duasestradas": {
+    -6.688338,
+    -35.417526
+  },
+  "paraiba-emas": {
+    -7.107145,
+    -37.715558
+  },
+  "paraiba-esperanca": {
+    -7.018517,
+    -35.852193
+  },
+  "paraiba-fagundes": {
+    -7.357747,
+    -35.783974
+  },
+  "paraiba-freimartinho": {
+    -6.403364,
+    -36.453514
+  },
+  "paraiba-gadobravo": {
+    -7.583052,
+    -35.791437
+  },
+  "paraiba-guarabira": {
+    -6.851398,
+    -35.491558
+  },
+  "paraiba-gurinhem": {
+    -7.124476,
+    -35.424854
+  },
+  "paraiba-gurjao": {
+    -7.247057,
+    -36.486271
+  },
+  "paraiba-ibiara": {
+    -7.499786,
+    -38.403499
+  },
+  "paraiba-imaculada": {
+    -7.387172,
+    -37.511681
+  },
+  "paraiba-inga": {
+    -7.290451,
+    -35.610994
+  },
+  "paraiba-itabaiana": {
+    -7.329957,
+    -35.335999
+  },
+  "paraiba-itaporanga": {
+    -7.304075,
+    -38.150578
+  },
+  "paraiba-itapororoca": {
+    -6.828301,
+    -35.244782
+  },
+  "paraiba-itatuba": {
+    -7.375851,
+    -35.626495
+  },
+  "paraiba-jacarau": {
+    -6.612305,
+    -35.291902
+  },
+  "paraiba-jerico": {
+    -6.553182,
+    -37.807819
+  },
+  "paraiba-joaopessoa": {
+    -7.121598,
+    -34.882028
+  },
+  "paraiba-juareztavora": {
+    -7.172693,
+    -35.590804
+  },
+  "paraiba-juazeirinho": {
+    -7.068828,
+    -36.580306
+  },
+  "paraiba-juncodoserido": {
+    -6.991929,
+    -36.713766
+  },
+  "paraiba-juripiranga": {
+    -7.374331,
+    -35.2396
+  },
+  "paraiba-juru": {
+    -7.537509,
+    -37.819227
+  },
+  "paraiba-lagoa": {
+    -6.589985,
+    -37.914901
+  },
+  "paraiba-lagoadedentro": {
+    -6.672881,
+    -35.378228
+  },
+  "paraiba-lagoaseca": {
+    -7.157099,
+    -35.854202
+  },
+  "paraiba-lastro": {
+    -6.517513,
+    -38.178764
+  },
+  "paraiba-livramento": {
+    -7.375744,
+    -36.944944
+  },
+  "paraiba-logradouro": {
+    -6.614788,
+    -35.443169
+  },
+  "paraiba-lucena": {
+    -6.898402,
+    -34.871192
+  },
+  "paraiba-maedagua": {
+    -7.258294,
+    -37.426977
+  },
+  "paraiba-malta": {
+    -6.906816,
+    -37.52171
+  },
+  "paraiba-mamanguape": {
+    -6.833698,
+    -35.121316
+  },
+  "paraiba-manaira": {
+    -7.706091,
+    -38.153125
+  },
+  "paraiba-marcacao": {
+    -6.770411,
+    -35.012148
+  },
+  "paraiba-mari": {
+    -7.058061,
+    -35.318416
+  },
+  "paraiba-marizopolis": {
+    -6.824404,
+    -38.341977
+  },
+  "paraiba-massaranduba": {
+    -7.181276,
+    -35.733071
+  },
+  "paraiba-mataraca": {
+    -6.59673,
+    -35.05308
+  },
+  "paraiba-matinhas": {
+    -7.121955,
+    -35.771343
+  },
+  "paraiba-matogrosso": {
+    -6.543386,
+    -37.714652
+  },
+  "paraiba-matureia": {
+    -7.292067,
+    -37.359134
+  },
+  "paraiba-mogeiro": {
+    -7.3019,
+    -35.478275
+  },
+  "paraiba-montadas": {
+    -7.088656,
+    -35.957737
+  },
+  "paraiba-montehorebe": {
+    -7.214284,
+    -38.585758
+  },
+  "paraiba-monteiro": {
+    -7.892018,
+    -37.123205
+  },
+  "paraiba-mulungu": {
+    -7.030939,
+    -35.464608
+  },
+  "paraiba-natuba": {
+    -7.641076,
+    -35.554273
+  },
+  "paraiba-nazarezinho": {
+    -6.915675,
+    -38.325108
+  },
+  "paraiba-novafloresta": {
+    -6.455431,
+    -36.204
+  },
+  "paraiba-novaolinda": {
+    -7.478242,
+    -38.040438
+  },
+  "paraiba-novapalmeira": {
+    -6.679504,
+    -36.418728
+  },
+  "paraiba-olhodagua": {
+    -7.228986,
+    -37.749007
+  },
+  "paraiba-olivedos": {
+    -6.991036,
+    -36.243729
+  },
+  "paraiba-ourovelho": {
+    -7.620909,
+    -37.151018
+  },
+  "paraiba-parari": {
+    -7.318172,
+    -36.655624
+  },
+  "paraiba-passagem": {
+    -7.136448,
+    -37.047429
+  },
+  "paraiba-patos": {
+    -7.025829,
+    -37.276682
+  },
+  "paraiba-paulista": {
+    -6.598036,
+    -37.621577
+  },
+  "paraiba-pedrabranca": {
+    -7.427755,
+    -38.069709
+  },
+  "paraiba-pedralavrada": {
+    -6.756274,
+    -36.467318
+  },
+  "paraiba-pedrasdefogo": {
+    -7.398898,
+    -35.112176
+  },
+  "paraiba-pianco": {
+    -7.198741,
+    -37.925576
+  },
+  "paraiba-picui": {
+    -6.510672,
+    -36.349274
+  },
+  "paraiba-pilar": {
+    -7.264034,
+    -35.252326
+  },
+  "paraiba-piloes": {
+    -6.868269,
+    -35.612974
+  },
+  "paraiba-piloezinhos": {
+    -6.842744,
+    -35.530835
+  },
+  "paraiba-pirpirituba": {
+    -6.780936,
+    -35.496713
+  },
+  "paraiba-pitimbu": {
+    -7.472806,
+    -34.811233
+  },
+  "paraiba-pocinhos": {
+    -7.076073,
+    -36.060447
+  },
+  "paraiba-pocodantas": {
+    -6.405017,
+    -38.496479
+  },
+  "paraiba-pocodejosedemoura": {
+    -6.57441,
+    -38.511457
+  },
+  "paraiba-pombal": {
+    -6.771132,
+    -37.798865
+  },
+  "paraiba-prata": {
+    -7.694884,
+    -37.083034
+  },
+  "paraiba-princesaisabel": {
+    -7.736898,
+    -37.992134
+  },
+  "paraiba-puxinana": {
+    -7.147522,
+    -35.961335
+  },
+  "paraiba-queimadas": {
+    -7.36404,
+    -35.901664
+  },
+  "paraiba-quixaba": {
+    -7.031688,
+    -37.147792
+  },
+  "paraiba-remigio": {
+    -6.968674,
+    -35.793213
+  },
+  "paraiba-pedroregis": {
+    -6.639906,
+    -35.28652
+  },
+  "paraiba-riachao": {
+    -6.540839,
+    -35.660895
+  },
+  "paraiba-riachaodobacamarte": {
+    -7.253472,
+    -35.669257
+  },
+  "paraiba-riachaodopoco": {
+    -7.147482,
+    -35.264755
+  },
+  "paraiba-riachodesantoantonio": {
+    -7.691407,
+    -36.156314
+  },
+  "paraiba-riachodoscavalos": {
+    -6.435588,
+    -37.656011
+  },
+  "paraiba-riotinto": {
+    -6.803828,
+    -35.077569
+  },
+  "paraiba-salgadinho": {
+    -7.102478,
+    -36.844681
+  },
+  "paraiba-salgadodesaofelix": {
+    -7.356537,
+    -35.438128
+  },
+  "paraiba-santacecilia": {
+    -7.744144,
+    -35.879329
+  },
+  "paraiba-santacruz": {
+    -6.533924,
+    -38.062
+  },
+  "paraiba-santahelena": {
+    -6.719891,
+    -38.639921
+  },
+  "paraiba-santaines": {
+    -7.626975,
+    -38.559241
+  },
+  "paraiba-santaluzia": {
+    -6.867785,
+    -36.918276
+  },
+  "paraiba-santanademangueira": {
+    -7.551652,
+    -38.336746
+  },
+  "paraiba-santanadosgarrotes": {
+    -7.385686,
+    -37.987565
+  },
+  "paraiba-jocaclaudino": {
+    -6.483215,
+    -38.478874
+  },
+  "paraiba-santarita": {
+    -7.128465,
+    -34.984619
+  },
+  "paraiba-santateresinha": {
+    -7.245407,
+    -35.849725
+  },
+  "paraiba-santoandre": {
+    -7.219859,
+    -36.631977
+  },
+  "paraiba-saobento": {
+    -6.485919,
+    -37.446933
+  },
+  "paraiba-saobentinho": {
+    -6.890094,
+    -37.72597
+  },
+  "paraiba-saodomingosdocariri": {
+    -7.635023,
+    -36.431743
+  },
+  "paraiba-saodomingos": {
+    -6.817432,
+    -37.942643
+  },
+  "paraiba-saofrancisco": {
+    -6.619307,
+    -38.095798
+  },
+  "paraiba-saojoaodocariri": {
+    -7.386747,
+    -36.528896
+  },
+  "paraiba-saojoaodotigre": {
+    -8.078237,
+    -36.849141
+  },
+  "paraiba-saojosedalagoatapada": {
+    -6.940713,
+    -38.123507
+  },
+  "paraiba-saojosedecaiana": {
+    -7.249591,
+    -38.299942
+  },
+  "paraiba-saojosedeespinharas": {
+    -6.84652,
+    -37.325023
+  },
+  "paraiba-saojosedosramos": {
+    -7.249042,
+    -35.380353
+  },
+  "paraiba-saojosedepiranhas": {
+    -7.119851,
+    -38.499135
+  },
+  "paraiba-saojosedeprincesa": {
+    -7.741214,
+    -38.098945
+  },
+  "paraiba-saojosedobonfim": {
+    -7.161612,
+    -37.30812
+  },
+  "paraiba-saojosedobrejodocruz": {
+    -6.212327,
+    -37.354454
+  },
+  "paraiba-saojosedosabugi": {
+    -6.775826,
+    -36.797916
+  },
+  "paraiba-saojosedoscordeiros": {
+    -7.391768,
+    -36.80795
+  },
+  "paraiba-saomamede": {
+    -6.923862,
+    -37.095436
+  },
+  "paraiba-saomigueldetaipu": {
+    -7.250409,
+    -35.209411
+  },
+  "paraiba-saosebastiaodelagoaderoca": {
+    -7.105168,
+    -35.867966
+  },
+  "paraiba-saosebastiaodoumbuzeiro": {
+    -8.152287,
+    -37.009072
+  },
+  "paraiba-sape": {
+    -7.093591,
+    -35.227975
+  },
+  "paraiba-saovicentedoserido": {
+    -6.933337,
+    -36.400864
+  },
+  "paraiba-serrabranca": {
+    -7.481527,
+    -36.663458
+  },
+  "paraiba-serradaraiz": {
+    -6.687433,
+    -35.442067
+  },
+  "paraiba-serragrande": {
+    -7.237157,
+    -38.385994
+  },
+  "paraiba-serraredonda": {
+    -7.186711,
+    -35.679795
+  },
+  "paraiba-serraria": {
+    -6.819313,
+    -35.638747
+  },
+  "paraiba-sertaozinho": {
+    -6.752366,
+    -35.440732
+  },
+  "paraiba-sobrado": {
+    -7.144288,
+    -35.23574
+  },
+  "paraiba-solanea": {
+    -6.754035,
+    -35.662194
+  },
+  "paraiba-soledade": {
+    -7.058292,
+    -36.366791
+  },
+  "paraiba-sossego": {
+    -6.767694,
+    -36.245508
+  },
+  "paraiba-sousa": {
+    -6.766203,
+    -38.231961
+  },
+  "paraiba-sume": {
+    -7.671521,
+    -36.878875
+  },
+  "paraiba-tacima": {
+    -6.489538,
+    -35.636485
+  },
+  "paraiba-taperoa": {
+    -7.206294,
+    -36.824499
+  },
+  "paraiba-tavares": {
+    -7.636494,
+    -37.878828
+  },
+  "paraiba-teixeira": {
+    -7.221036,
+    -37.252477
+  },
+  "paraiba-tenorio": {
+    -6.940837,
+    -36.627796
+  },
+  "paraiba-triunfo": {
+    -6.579042,
+    -38.596986
+  },
+  "paraiba-uirauna": {
+    -6.521424,
+    -38.412065
+  },
+  "paraiba-umbuzeiro": {
+    -7.696494,
+    -35.666623
+  },
+  "paraiba-varzea": {
+    -6.771058,
+    -36.99121
+  },
+  "paraiba-vieiropolis": {
+    -6.543364,
+    -38.277819
+  },
+  "paraiba-zabele": {
+    -8.075514,
+    -37.098513
+  }
+}
